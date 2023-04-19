f89412caa5ce53dce0efd33b46151d6230c60b05
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2023-04-19 13:44:44 +0200

    take a list of ase obj instead of xyz file - more flexible

--- rholearn/qchem.py
+++ rholearn/qchem.py
@@ -1,33 +1,33 @@
 import os
+from typing import List
+
+import ase
 import ase.io
 
 
-def generate_aims_input_geometry_files(xyz_file: str, save_dir: str):
+def generate_aims_input_geometry_files(frames: List[ase.Atoms], save_dir: str):
     """
-    Takes an `xyz_file` (i.e. of .xyz format) that contains the coordinates for
-    a set of structures and generates input geometry files in the AIMS format.
+    Takes a list of ASE Atoms objects (i.e. ``frames``) for a set of structures
+    and generates input geometry files in the AIMS format.
 
-    For a set of N structures in `xyz_file`, N new directories in the parent
-    directory `save_dir` are created, with relative paths
+    For a set of N structures in ``frames``, N new directories in the parent
+    directory ``save_dir`` are created, with relative paths
     f"{save_dir}/{A}/geometry.in", where A is a numeric structure index running
     from 0 -> (N - 1) (inclusive), and corresponding to the order of structures
-    in `xyz_file`.
+    in ``frames``.
 
-    :param xyz_file: a `str` of the absolute path to the xyz file containing the
-        geometries.
+    :param frames: a :py:class:`list` of :py:class:`ase.Atoms` objects
+        corresponding to the structures in the dataset to generate AIMS input
+        files for.
     :param save_dir: a `str` of the absolute path to the directory where the
         AIMS input geometry files should be saved.
     """
-
-    # Read the xyz file into frames using ASE
-    frames = ase.io.read(xyz_file, ":")
-    N = len(frames)
 
     # Create the save directory if it doesn't already exist
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
 
-    for A in range(N):  # Iterate over structures
+    for A in range(len(frames)):  # Iterate over structures
         # Create a directory named simply by the structure index
         structure_dir = os.path.join(save_dir, f"{A}")
         if not os.path.exists(structure_dir):
