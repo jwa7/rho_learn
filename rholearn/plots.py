9a916697de87e0c177f1f4948823089458468ba9
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2023-05-02 11:07:15 +0200

    coefficient histogram plots

--- rholearn/plots.py
+++ rholearn/plots.py
@@ -2,7 +2,7 @@
 Functions to buidl analysis plots using matplotlib
 """
 from itertools import product
-from typing import List, Dict, Union
+from typing import List, Dict, Union, Optional
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,7 +11,7 @@ import mpltex
 import equistore
 from equistore import TensorMap
 
-from rholearn import utils
+from rholearn import error, utils
 
 
 def loss_vs_epoch(
@@ -149,6 +149,199 @@ def parity_plot(
     fig.tight_layout()
 
     return fig, ax
+
+
+def plot_coeff_hist(
+    tensors: Union[TensorMap, List[TensorMap]],
+    nbins: Optional[float] = 100,
+    epsi: Optional[float] = None,
+):
+    """
+    Plots the histogram of the absolute value of the coefficients of
+    ``tensors``. The rows of the subplot correspond to the different L values,
+    while the columns correspond to the different tensors passed.
+
+    Histograms are plotted on a log-log scale, such that only coefficients that
+    are greater than zero are plotted.
+
+    Returns the matplotlib figure and axes, as well as a dict of the flattened
+    coefficients for each l value.
+    """
+    # Check the input tensors
+    if isinstance(tensors, TensorMap):
+        tensors = [tensors]
+    if len(tensors) > 1:
+        for tensor in tensors[1:]:
+            if not equistore.equal_metadata(tensors[0], tensor):
+                raise ValueError(
+                    "metadata of all tensors must be equal to plot them together"
+                )
+
+    # Get the list of L values
+    L = np.sort(np.unique(tensors[0].keys["spherical_harmonics_l"]))
+
+    # Compile flattened vectors of absolute nonzero coefficients for each l
+    # value
+    results = [{l: np.array([]) for l in L} for tensor in tensors]
+    for tensor_i, tensor in enumerate(tensors):
+        for key, block in tensor:
+            l = key["spherical_harmonics_l"]
+            vals = block.values.flatten()
+            results[tensor_i][l] = np.concatenate([results[tensor_i][l], vals])
+
+    # Build the subplots
+    fig, axes = plt.subplots(
+        len(L),
+        len(tensors),
+        figsize=(4 * len(tensors), 3 * len(L)),
+        sharex=True,
+        sharey=True,
+    )
+    # Plot histograms with each l on a different row, and each tensor in a different column
+    for tensor_i in range(len(tensors)):
+        for l in L:
+            # Take the absolute and nonzero values
+            vals = np.abs(results[tensor_i][l])
+            vals = vals[vals > 0]
+            # Compute the bins sizes in log space
+            logbins = np.logspace(np.log10(np.min(vals)), np.log10(np.max(vals)), nbins)
+            # Plot and set axes to log scale
+            if len(tensors) == 1:
+                axes[l].hist(vals, bins=logbins, ec="k")
+                axes[l].set_xscale("log")
+                axes[l].set_yscale("log")
+            else:
+                axes[l, tensor_i].hist(vals, bins=logbins, ec="k")
+                axes[l, tensor_i].set_xscale("log")
+                axes[l, tensor_i].set_yscale("log")
+
+    # Add row titles for the L values
+    pad = 5
+    row_titles = [f"L = {l}" for l in L]
+    tmp_axes = axes if len(tensors) == 1 else axes[:, 0]
+    for ax, row_title in zip(tmp_axes, row_titles):
+        ax.annotate(
+            row_title,
+            xy=(0, 0.5),
+            xytext=(-ax.yaxis.labelpad - pad, 0),
+            xycoords=ax.yaxis.label,
+            textcoords="offset points",
+            size="large",
+            ha="right",
+            va="center",
+        )
+    # Add column titles
+    if len(tensors) > 1:
+        col_titles = [f"tensor {tensor_i}" for tensor_i in range(len(tensors))]
+        for ax, col_title in zip(axes[0], col_titles):
+            ax.annotate(
+                col_title,
+                xy=(0.5, 1),
+                xytext=(0, pad),
+                xycoords="axes fraction",
+                textcoords="offset points",
+                size="large",
+                ha="center",
+                va="baseline",
+            )
+    fig.tight_layout()
+    fig.subplots_adjust(left=0.15, top=0.95)
+
+    return fig, ax, results
+
+
+def plot_comparison_hist(
+    a: TensorMap,
+    b: TensorMap,
+    nbins: Optional[float] = 100,
+    epsi: Optional[float] = None,
+):
+    """
+    Calcualtes the relative error between two TensorMaps ``a`` and ``b``, only
+    for elements in both ``a`` and ``b`` that are greater than ``epsi``. Plots
+    the values of ``a``, ``b``, and the relative error in a 3 column plot. Each
+    row corresponds to a different l value.
+
+    Returns the matplotlib figure and axes objects, and the results dict.
+    """
+
+    # First, calculate the relative errors
+    results = error.relative_errors_a_b(a, b, epsi)
+
+    # Get the list of L values
+    L = np.sort(list(results.keys()))
+
+    # Build the subplots
+    fig, axes = plt.subplots(
+        len(L), 3, figsize=(12, 3 * len(L)), sharex="col", sharey=True
+    )
+    for l in L:
+        # First column - a values
+        a_vals = results[l]["a_vals"]
+        a_vals = a_vals[a_vals > 0]
+        logbins_a = np.logspace(
+            np.log10(np.min(a_vals)), np.log10(np.max(a_vals)), nbins
+        )
+        axes[l, 0].hist(a_vals, bins=logbins_a, ec="k")
+        # Second column - b values
+        b_vals = results[l]["b_vals"]
+        b_vals = b_vals[b_vals > 0]
+        logbins_b = np.logspace(
+            np.log10(np.min(b_vals)), np.log10(np.max(b_vals)), nbins
+        )
+        axes[l, 1].hist(b_vals, bins=logbins_b, ec="k")
+        # Third column - relative error
+        err_vals = results[l]["rel_error"]
+        err_vals = err_vals[err_vals > 0]
+        if len(err_vals) > 0:
+            logbins_err = np.logspace(
+                np.log10(np.min(err_vals)), np.log10(np.max(err_vals)), nbins
+            )
+            axes[l, 2].hist(err_vals, bins=logbins_err, ec="k")
+        # Plot epsi if passed
+        if epsi is not None:
+            axes[l, 0].axline((epsi, 0), (epsi, 1), color="grey", linestyle="dashed")
+            axes[l, 1].axline((epsi, 0), (epsi, 1), color="grey", linestyle="dashed")
+
+    # Set log scales
+    for row in axes:
+        for ax in row:
+            ax.set_xscale("log")
+            ax.set_yscale("log")
+
+    # Add row titles for the L values
+    pad = 5
+    row_titles = [f"L = {l}" for l in L]
+    for ax, row_title in zip(axes[:, 0], row_titles):
+        ax.annotate(
+            row_title,
+            xy=(0, 0.5),
+            xytext=(-ax.yaxis.labelpad - pad, 0),
+            xycoords=ax.yaxis.label,
+            textcoords="offset points",
+            size="large",
+            ha="right",
+            va="center",
+        )
+
+    # Add column titles
+    col_titles = ["|a|", "|b|", "relative error: | (a - b) / (a + b) |"]
+    for ax, col_title in zip(axes[0], col_titles):
+        ax.annotate(
+            col_title,
+            xy=(0.5, 1),
+            xytext=(0, pad),
+            xycoords="axes fraction",
+            textcoords="offset points",
+            size="large",
+            ha="center",
+            va="baseline",
+        )
+
+    fig.tight_layout()
+    fig.subplots_adjust(left=0.15, top=0.95)
+
+    return fig, ax, results
 
 
 @mpltex.presentation_decorator
