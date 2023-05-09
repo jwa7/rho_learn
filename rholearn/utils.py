722d6a0a303627f7b8abc8830f3d8606a4d2dac4
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2023-05-09 14:32:55 +0200

    remove `utils.drop_blocks` as now in equistore

--- rholearn/utils.py
+++ rholearn/utils.py
@@ -668,44 +668,6 @@ def sort_tensormap(tensor: TensorMap) -> TensorMap:
     return TensorMap(keys=keys, blocks=blocks)
 
 
-def drop_blocks(tensor: TensorMap, keys: Labels) -> TensorMap:
-    """
-    Drop specified key/block pairs from a TensorMap.
-
-    :param tensor: the TensorMap to drop the key-block pair from.
-    :param keys: a :py:class:`Labels` object containing the keys of the
-        blocks to drop
-
-    :return: the input :py:class:`TensorMap` with the specified key/block
-        pairs dropped.
-    """
-    if not np.all(tensor.keys.names == keys.names):
-        raise ValueError(
-            "The input tensor's keys must have the same names as the specified"
-            f" keys to drop. Should be {tensor.keys.names} but got {keys.names}"
-        )
-    diff = np.setdiff1d(keys, tensor.keys)
-    if len(diff) > 0:
-        raise ValueError(
-            "some keys in `keys` are not present in `tensor`."
-            f" Non-existent keys: {diff}"
-        )
-    new_keys = np.setdiff1d(tensor.keys, keys)
-    # new_blocks = [tensor[key].copy() for key in new_keys]
-    new_blocks = [
-        TensorBlock(
-            values=tensor[key].values,
-            samples=tensor[key].samples,
-            components=tensor[key].components,
-            properties=tensor[key].properties,
-        )
-        for key in new_keys
-    ]
-    del tensor
-    trim_memory()
-    return TensorMap(keys=new_keys, blocks=new_blocks)
-
-
 # ===== other utility functions
 
 
