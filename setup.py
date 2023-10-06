4e978f79d55ac9a3121023781a85fcc391c8b11d
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2023-10-06 18:38:40 +0200

    Changes to setup

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="rholearn",
+    name="rho_learn",
     version="0.0.0",
     packages=find_packages(
         include=[
+            "rhocalc",
+            "rhocalc.*",
             "rholearn", 
             "rholearn.*",
-            "rhoparse",
-            "rhoparse.*",
-            "docs.example.azoswitch",
+            "rhopredict",
+            "rhopredict.*"
             ]
         ),
 )
