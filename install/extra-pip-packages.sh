e3d74cadaa961eb3603d7fda8c323f87996f18a5
Joseph Abbott <joseph.william.abbott@gmail.com>
2023-11-24 16:43:33 +0100

    Need dev version of ASE

--- install/extra-pip-packages.sh
+++ install/extra-pip-packages.sh
@@ -12,5 +12,5 @@ pip cache remove "rascaline*"
 
 # Has to be in this order, such that the recent changes in metatensor overrides
 # the older metatensor version installed by rascaline
-pip install --no-build-isolation git+https://github.com/luthaf/rascaline@6dc73889a6adcf2e34fb24967cbd039f45055bbe
+pip install --no-build-isolation git+https://github.com/luthaf/rascaline@148ed002074de6c1cd2130fbbb1bbe0ddd43ed4d
 pip install --no-build-isolation git+https://github.com/lab-cosmo/metatensor@659ce8dc6cab85cbea95371a4f841cd3bd686b54
