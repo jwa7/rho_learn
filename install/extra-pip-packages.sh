a5bf7aa27c1bc759ae0ea7e97478c11252447275
Joseph Abbott <joseph.william.abbott@gmail.com>
2024-02-20 19:04:03 +0100

    Update pinned version

--- install/extra-pip-packages.sh
+++ install/extra-pip-packages.sh
@@ -2,29 +2,33 @@
 
 set -eux
 
-# PyTorch
-pip install --extra-index-url https://download.pytorch.org/whl/cpu torch
+# ===== PyTorch
+pip install --extra-index-url https://download.pytorch.org/whl/cpu torch==2.2
 
-# A fork of ASE is needed, as this contains a bug fix for the FHI-aims ASE
-# calculator
+# ===== ASE 
+# ===== A fork is needed, as this contains a bug fix for the FHI-aims ASE calculator
 pip uninstall -y ase
 pip cache remove "ase*"
 pip install git+https://gitlab.com/jwa7/ase@977968238d011a795a417f9791fa816a80d20d87
 
-# 'Cube-Toolz' for reading and manipulating cube files
+# ===== 'Cube-Toolz' for reading and manipulating cube files
 pip uninstall -y cube_tools
 pip cache remove "cube_tools*"
 pip install git+https://github.com/funkymunkycool/Cube-Toolz.git
 
-# Software from the COSMO stack
+# ===== chemiscope
+pip cache remove "chemiscope*"
 pip install chemiscope
 
-pip uninstall -y metatensor metatensor-core metatensor-operations metatensor-torch metatensor-learn
-pip uninstall -y rascaline rascaline-torch
-pip cache remove "metatensor*"
-pip cache remove "rascaline*"
+# ===== rascaline
+# pip uninstall -y rascaline rascaline-torch
+# pip cache remove "rascaline*"
+# pip install --no-build-isolation git+https://github.com/luthaf/rascaline@96cee2a34f8b091fbd7be539ace3ed529216f0ef
+# pip install --no-build-isolation git+https://github.com/luthaf/rascaline@96cee2a34f8b091fbd7be539ace3ed529216f0ef#subdirectory=python/rascaline-torch
 
-# Has to be in this order, such that the recent changes in metatensor overrides
-# the older metatensor version installed by rascaline
-pip install --no-build-isolation git+https://github.com/luthaf/rascaline@02fff56f327f1fd8b31f0437638e7af4b34127bd
-pip install --no-build-isolation git+https://github.com/lab-cosmo/metatensor@bec4561db93ed9a8037db6d39d264ebccb62c25d
+
+# ===== metatensor
+pip uninstall -y metatensor metatensor-core metatensor-operations metatensor-torch metatensor-learn
+pip cache remove "metatensor*"
+pip install --no-build-isolation git+https://github.com/lab-cosmo/metatensor@015d6d6eeef03ed4ab27f9d8a3a77e2f54356da9
+pip install --no-build-isolation git+https://github.com/lab-cosmo/metatensor@015d6d6eeef03ed4ab27f9d8a3a77e2f54356da9#subdirectory=python/metatensor-torch
