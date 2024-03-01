f846b6000896f7cf8621331ac8f4c8ee294dc603
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2024-03-01 18:50:41 +0100

    k weights!

--- rhocalc/aims/aims_fields.py
+++ rhocalc/aims/aims_fields.py
@@ -168,8 +168,8 @@ def get_kso_weight_vector_ldos(
     kso_info_path: str, gaussian_width: float, target_energy: float = None
 ) -> np.ndarray:
     """
-    Returns the KS-orbital weight vector for constructing the Local Density of
-    States (LDOS).
+    Returns the KS-orbital weight vector for constructing the Local Density of States
+    (LDOS).
 
     Each KSO weight is given by the evaluation of a Gaussian function, of width
     `gaussian_width`, centered on the KS-orbital eigenvalue and evaluated at the
@@ -188,10 +188,11 @@ def get_kso_weight_vector_ldos(
 
     return np.array(
         [
-            evaluate_gaussian(
-                target=target_energy, center=kso_eig, width=gaussian_width
+            kso["k_weight"]
+            * evaluate_gaussian(
+                target=target_energy, center=kso["energy_eV"], width=gaussian_width
             )
-            for kso_eig in kso_info["energy_eV"]
+            for kso in kso_info
         ]
     )
 
@@ -204,19 +205,18 @@ def get_kso_weight_vector_ildos(
     target_energy: float = None,
 ) -> np.ndarray:
     """
-    Returns the KS-orbital weight vector for constructing the Local Density of
-    States (LDOS).
+    Returns the KS-orbital weight vector for constructing the Local Density of States
+    (LDOS).
 
     Each KSO weight is given by the sum of Gaussian functions, each of width
-    `gaussian_width`, centered on the KS-orbital eigenvalue and evaluated on a
-    discrete energy grid from the `target_energy` to `target_energy` +
-    `biasing_voltage`.
+    `gaussian_width`, centered on the KS-orbital eigenvalue and evaluated on a discrete
+    energy grid from the `target_energy` to `target_energy` + `biasing_voltage`.
 
     Where `target_energy` is not passed, the eigenvalue of the highest occupied
     KS-orbital is used.
 
-    `biasing_voltage`, `gaussian_width`, and `target_energy`  must be passed in
-    units of eV.
+    `biasing_voltage`, `gaussian_width`, and `target_energy`  must be passed in units of
+    eV.
     """
     kso_info = aims_parser.get_ks_orbital_info(kso_info_path)
 
@@ -228,23 +228,25 @@ def get_kso_weight_vector_ildos(
         [
             np.mean(
                 [
-                    evaluate_gaussian(
-                        target=tmp_target_energy, center=kso_eig, width=gaussian_width
+                    kso["k_weight"]
+                    * evaluate_gaussian(
+                        target=tmp_target_energy,
+                        center=kso["energy_eV"],
+                        width=gaussian_width,
                     )
                     for tmp_target_energy in np.linspace(
                         target_energy, biasing_voltage, energy_grid_points
                     )
                 ]
             )
-            for kso_eig in kso_info["energy_eV"]
+            for kso in kso_info
         ]
     )
 
 
 def evaluate_gaussian(target: float, center: float, width: float) -> float:
     """
-    Evaluates a Gaussian function with the specified parameters at the target
-    value
+    Evaluates a Gaussian function with the specified parameters at the target value
     """
     return (1.0 / (width * np.sqrt(2 * np.pi))) * np.exp(
         -0.5 * ((target - center) / width) ** 2
@@ -257,23 +259,23 @@ def evaluate_gaussian(target: float, center: float, width: float) -> float:
 #     from file "ks_orbital_info.out" at path `kso_info_path` and plots the sum of
 #     these as the Density of States (DOS).
 #     """
-    # import matplotlib.pyplot as plt
-    # from rholearn import utils
+# import matplotlib.pyplot as plt
+# from rholearn import utils
 
-    # width = 0.3
-    # # e_grid = np.linspace(np.min(kso_info["energy_eV"]) - 20, np.max(kso_info["energy_eV"]) + 20, 10000)
-    # e_grid = np.linspace(-20, 5, 1000)
+# width = 0.3
+# # e_grid = np.linspace(np.min(kso_info["energy_eV"]) - 20, np.max(kso_info["energy_eV"]) + 20, 10000)
+# e_grid = np.linspace(-20, 5, 1000)
 
-    # fig, ax = plt.subplots()
+# fig, ax = plt.subplots()
 
-    # dos = np.zeros(len(e_grid))
-    # kso_weights = []
-    # for e_center in kso_info["energy_eV"]:
-    #     gauss = utils.evaluate_gaussian(target=e_grid, center=e_center, width=width)
-    #     kso_weights.append(utils.evaluate_gaussian(target=e_target, center=e_center, width=width))
-    #     dos += gauss
-    #     ax.plot(gauss, e_grid)
-    # ax.plot(dos + 5, e_grid, color='k')
-    # ax.axline((0, e_target), (10, e_target))
-    # ax.set_ylabel("Energy (eV)")
-    # # ax.set_xlim([-150, 10])
+# dos = np.zeros(len(e_grid))
+# kso_weights = []
+# for e_center in kso_info["energy_eV"]:
+#     gauss = utils.evaluate_gaussian(target=e_grid, center=e_center, width=width)
+#     kso_weights.append(utils.evaluate_gaussian(target=e_target, center=e_center, width=width))
+#     dos += gauss
+#     ax.plot(gauss, e_grid)
+# ax.plot(dos + 5, e_grid, color='k')
+# ax.axline((0, e_target), (10, e_target))
+# ax.set_ylabel("Energy (eV)")
+# # ax.set_xlim([-150, 10])
