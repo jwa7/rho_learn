1deaa5527061f432b7f625a7c6e0408751cb10a8
Joseph Abbott <joseph.william.abbott@gmail.com>
2024-05-24 11:08:20 +0200

    Update metatensor-learn

--- install/extra-pip-packages.sh
+++ install/extra-pip-packages.sh
@@ -4,18 +4,18 @@ set -eux
 
 # ===== ASE 
 # ===== A fork is needed, as this contains a bug fix for the FHI-aims ASE calculator
-# pip uninstall -y ase
-# pip cache remove "ase*"
-# pip install git+https://gitlab.com/jwa7/ase@977968238d011a795a417f9791fa816a80d20d87
+pip uninstall -y ase
+pip cache remove "ase*"
+pip install git+https://gitlab.com/jwa7/ase@977968238d011a795a417f9791fa816a80d20d87
 
-# # ===== 'Cube-Toolz' for reading and manipulating cube files
-# pip uninstall -y cube_tools
-# pip cache remove "cube_tools*"
-# pip install git+https://github.com/funkymunkycool/Cube-Toolz.git
+# ===== 'Cube-Toolz' for reading and manipulating cube files
+pip uninstall -y cube_tools
+pip cache remove "cube_tools*"
+pip install git+https://github.com/funkymunkycool/Cube-Toolz.git
 
-# # ===== chemiscope
-# pip cache remove "chemiscope*"
-# pip install chemiscope
+# ===== chemiscope
+pip cache remove "chemiscope*"
+pip install chemiscope
 
 # ===== Uninstalls, clear caches
 pip uninstall -y torch rascaline rascaline-torch metatensor metatensor-core metatensor-operations metatensor-torch metatensor-learn wigners
@@ -34,4 +34,4 @@ pip install --no-build-isolation --no-deps "rascaline-torch @ git+https://github
 pip install --no-build-isolation --no-deps metatensor-operations
 
 # ===== metatensor-learn latest version
-pip install --no-build-isolation --no-deps "metatensor-learn @ git+https://github.com/lab-cosmo/metatensor@590e3ac087596f4b44f8ba6270f5424d6bb5e61f#subdirectory=python/metatensor-learn"
+pip install --no-build-isolation --no-deps "metatensor-learn @ git+https://github.com/lab-cosmo/metatensor@8263f36659135d23a1d6590fb307898a2551b52e#subdirectory=python/metatensor-learn"
