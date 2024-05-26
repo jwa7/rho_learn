9c3dcd9b41888775c94ff0da8ced468f258673d6
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2024-05-26 10:35:20 +0200

    run scripts

---
+++ example/scripts/eval.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## ===== Comment out slurm directives when running locally =====
+#SBATCH --job-name=eval
+#SBATCH --nodes=1
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --partition=standard
+#SBATCH --ntasks-per-node=20
+#SBATCH --output=./slurm_out/slurm_eval.out
+#SBATCH --get-user-env
+
+python3 -c 'from rholearn import eval; from dft_settings import DFT_SETTINGS; from ml_settings import ML_SETTINGS; eval.eval(DFT_SETTINGS, ML_SETTINGS)'
---
+++ example/scripts/train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## ===== Comment out slurm directives when running locally =====
+#SBATCH --job-name=train
+#SBATCH --nodes=1
+#SBATCH --time=1:00:00
+#SBATCH --mem-per-cpu=2000
+#SBATCH --partition=standard
+#SBATCH --ntasks-per-node=20
+#SBATCH --output=./slurm_out/slurm_train.out
+#SBATCH --get-user-env
+
+python3 -c 'from rholearn import train; from ml_settings import ML_SETTINGS; train.train(ML_SETTINGS)'
