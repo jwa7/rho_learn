8f52b909a818c6e45f1bc20829a117ab2b416280
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2024-06-25 17:13:54 +0200

    Update settings

--- example/settings/dft_settings.py
+++ example/settings/dft_settings.py
@@ -9,26 +9,24 @@ import ase.io
 import numpy as np
 
 # ===== SETUP =====
-SEED = 42
-DATA_DIR = "/work/cosmo/abbott/april-24/si_masked/data"
-FIELD_NAME = "ildos"
-RI_FIT_ID = "ildos-1V"
+DATA_DIR = "/work/cosmo/abbott/june-24/ArgAu/data"
+FIELD_NAME = "edensity"
+RI_FIT_ID = "edensity"
+# FIELD_NAME = "ildos"
+# RI_FIT_ID = "ildos"
 
 # ===== DATA =====
-ALL_SYSTEM = ase.io.read(
-    join(DATA_DIR, "si_slabs_dimer_z_depth_geomopt_distorted.xyz"), ":"
-)
+ALL_SYSTEM = ase.io.read(join(DATA_DIR, "ArgHAu_geomopt_energies_forces.xyz"), ":")
 ALL_SYSTEM_ID = np.arange(len(ALL_SYSTEM))
-# np.random.default_rng(seed=SEED).shuffle(ALL_SYSTEM_ID)  # shuffle first?
-SYSTEM_ID = ALL_SYSTEM_ID[6::13]
+SYSTEM_ID = np.array(list(ALL_SYSTEM_ID[1:100]) + list(ALL_SYSTEM_ID[721:821]))
+# SYSTEM_ID = np.array([0])
 SYSTEM = [ALL_SYSTEM[A] for A in SYSTEM_ID]
-
 
 # ===== HPC
 SBATCH = {
-    "job-name": "si_mask",
+    "job-name": "ArgAu",
     "nodes": 1,
-    "time": "8:00:00",
+    "time": "02:00:00",
     "mem-per-cpu": 0,
     "partition": "standard",
     "ntasks-per-node": 72,
@@ -47,28 +45,50 @@ HPC = {
 # ===== BASE AIMS =====
 AIMS_PATH = "/home/abbott/codes/new_aims/FHIaims/build/aims.230905.scalapack.mpi.x"
 BASE_AIMS = {
-    "species_dir": "/home/abbott/codes/rho_learn/rhocalc/aims/aims_species/tight/default",
+    "species_dir": "/home/abbott/codes/rho_learn/rhocalc/aims/aims_species/light/modified",
+    # Physical settings
     "xc": "pbe",
     "spin": "none",
-    # "relativistic": ["atomic_zora scalar"],
     "charge": 0,
-    "sc_accuracy_rho": 1e-8,
-    "wave_threshold": 1e-8,
-    "k_grid_density": 12,
+    "relativistic": ["atomic_zora scalar"],
+    "k_grid": [4, 4, 1],
+    # Computational
+    # "collect_eigenvectors": False,
+    # "cpu_consistency_threshold": 1e-11,
+    # "check_cpu_consistency": False,
+    # "empty_states": 10,
 }
 
 # ===== SCF =====
 SCF = {
-    "elsi_restart": "read",
+    "elsi_restart": "write 1000",  # set to higher number here? Writes upon convergence in all cases.
     "ri_fit_write_orbital_info": True,
     # "evaluate_work_function": True,
     "output": [
         "atom_proj_dos -30 5 1000 0.3",
-        "cube hartree_potential",
+        # "cube hartree_potential",
     ],  # atom_proj_dos Estart Eend n_points broadening
     # Geometry Optimization. Use "light" for pre-relaxation.
     # "relax_geometry": ["bfgs 0.01"],
     # "species_dir": "/home/abbott/march-24/rho_learn/rhocalc/aims/aims_species/light/default",  # jed
+    # SC accuracy
+    "sc_accuracy_rho": 2e-4,
+    "sc_accuracy_eev": 2e-3,
+    "sc_accuracy_etot": 1e-6,
+    # "sc_accuracy_forces": 2e-3,
+    "sc_iter_limit": 10000,
+    # Dispersion and dipoles
+    "vdw_correction_hirshfeld": True,
+    "vdw_pair_ignore": ["Au Au"],
+    # params for vdW-surf for Au !!! species sub-tag needed?
+    # "hirshfeld_param": [134, 15.6, 2.91],
+    "use_dipole_correction": True,
+    # Mixing
+    "mixer": "pulay",
+    "n_max_pulay": 10,
+    "charge_mix_param": 0.1,
+    "hartree_convergence_parameter": 5,
+    "occupation_type gaussian": 0.1,
 }
 
 # ===== RI =====
@@ -78,13 +98,13 @@ RI = {
     "sc_iter_limit": 0,
     "postprocess_anyway": True,
     # ===== What we want to fit to:
-    "ri_fit_field_from_kso_weights": True,  # build custom scalar field
-    # "ri_fit_total_density": True,
+    # "ri_fit_field_from_kso_weights": True,  # build custom scalar field
+    "ri_fit_total_density": True,
     # ===== Specific setting for RI fitting
     "ri_fit_ovlp_cutoff_radius": 1.5,
     "ri_fit_assume_converged": True,
-    "default_max_l_prodbas": 3,
-    # "default_max_n_prodbas": 6,  # currently doesn't work in FHI-aims
+    "default_max_l_prodbas": 1,
+    # "default_max_n_prodbas": 3,
     # ===== What to write as output
     "ri_fit_write_coeffs": True,  # RI coeffs (the learning target)
     "ri_fit_write_ovlp": True,  # RI overlap (needed for loss evaluation)
@@ -92,8 +112,13 @@ RI = {
     "ri_fit_write_rebuilt_field": True,  # RI-rebuilt scalar field on AIMS grid
     "ri_fit_write_ref_field_cube": True,  # SCF converged scalar field on CUBE grid
     "ri_fit_write_rebuilt_field_cube": True,  # RI-rebuilt scalar field on CUBE grid
-    "output": ["cube ri_fit"],  # Allows output of cube files
+    "output": ["cube total_density", "cube ri_fit"],  # Allows output of cube files
 }
+PROCESS_WHAT = []
+if "ri_fit_write_coeffs" in RI:
+    PROCESS_WHAT.append("coeffs")
+if "ri_fit_write_ovlp" in RI:
+    PROCESS_WHAT.append("ovlp")
 
 # ===== REBUILD =====
 REBUILD = {
@@ -115,20 +140,45 @@ REBUILD = {
 
 # ===== FIELD AND CUBE =====
 LDOS = {
-    "target_energy": "fermi_integrated_eV",  # "fermi_integrated_eV", "vbm_eV", "fermi_eV"
-    "gaussian_width": 0.3,  # eV
-    "biasing_voltage": -1.0,  # V, for the integrated LDOS
+    "target_energy": "fermi_eV",  # "fermi_integrated_eV", "vbm_eV", "fermi_eV"
+    "gaussian_width": 0.01,  # eV
+    "biasing_voltage": 0.01,  # V, for the integrated LDOS
     "method": "gaussian_analytical",
+}
+# MASK = None
+MASK = {
+    "surface_depth": 2.0,  # Ang
+    "buffer_depth": 2.0,  # Ang
 }
 CUBE = {
     "slab": True,
-    "n_points": (100, 100, 100),  # number of cube edge points
+    "n_points": (200, 200, 100),  # number of cube edge points
+    # "z_min": - MASK["surface_depth"] - MASK["buffer_depth"],  # Ang
+    # "z_max": None,
+}
+STM = {
+    "mode": "ccm",
+    "isovalue": 0.01,
+    "tolerance": 1e-3,
+    "grid_multiplier": 20,
+    "z_min": -1,
+    "z_max": None,
+    "xy_tiling": [1, 1],
+    "levels": 50,
 }
 
 # ===== RELATIVE DIRS =====
-SCF_DIR = lambda A: join(DATA_DIR, f"{A}")
-RI_DIR = lambda A: join(SCF_DIR(A), RI_FIT_ID)
-REBUILD_DIR = lambda A: join(RI_DIR(A), "rebuild")
+SCF_DIR = lambda A: join(DATA_DIR, f"{A}", "scf")
+RI_DIR = lambda A: join(DATA_DIR, f"{A}", "ri", RI_FIT_ID)
+REBUILD_DIR = lambda A: join(
+    RI_DIR(A),
+    "rebuild",
+    (
+        "unmasked"
+        if MASK is None
+        else f"masked_{MASK['surface_depth']}_{MASK['buffer_depth']}"
+    ),
+)
 PROCESSED_DIR = lambda A: join(RI_DIR(A), "processed")
 
 # ===== CREATE DIRS =====
@@ -137,7 +187,6 @@ if not exists(DATA_DIR):
 
 # ===== FINAL DICT =====
 DFT_SETTINGS = {
-    "SEED": SEED,
     "DATA_DIR": DATA_DIR,
     "FIELD_NAME": FIELD_NAME,
     "RI_FIT_ID": RI_FIT_ID,
@@ -151,7 +200,14 @@ DFT_SETTINGS = {
     "BASE_AIMS": BASE_AIMS,
     "SCF": SCF,
     "RI": RI,
+    "PROCESS_WHAT": PROCESS_WHAT,
     "REBUILD": REBUILD,
     "LDOS": LDOS,
+    "MASK": MASK,
     "CUBE": CUBE,
+    "STM": STM,
+    "SCF_DIR": SCF_DIR,
+    "RI_DIR": RI_DIR,
+    "REBUILD_DIR": REBUILD_DIR,
+    "PROCESSED_DIR": PROCESSED_DIR,
 }
--- example/settings/ml_settings.py
+++ example/settings/ml_settings.py
@@ -14,29 +14,30 @@ import metatensor.torch as mts
 
 from metatensor.torch.learn import nn
 
+from rhocalc import convert
 from rholearn.loss import L2Loss
 from rholearn.utils import timestamp, unpickle_dict
+
+from dft_settings import SCF_DIR, RI_DIR, PROCESSED_DIR, REBUILD_DIR, MASK
 
 # ===== SETUP =====
 SEED = 42
 FIELD_NAME = "edensity"
 RI_FIT_ID = "edensity"
-DATA_DIR = "/work/cosmo/abbott/april-24/si_masked/data"
-ML_DIR = "/home/abbott/may-24/si_clean_distortions/run_1/masked/2"
+DATA_DIR = "/work/cosmo/abbott/june-24/ArgAu/data"
+ML_DIR = os.getcwd()
 
 # ===== DATA =====
-ALL_SYSTEM = ase.io.read(
-    join(DATA_DIR, "si_slabs_dimer_z_depth_geomopt_distorted.xyz"), ":"
-)
+ALL_SYSTEM = ase.io.read(join(DATA_DIR, "ArgHAu_geomopt_energies_forces.xyz"), ":")
 ALL_SYSTEM_ID = np.arange(len(ALL_SYSTEM))
-# np.random.default_rng(seed=SEED).shuffle(ALL_SYSTEM_ID)  # shuffle first?
-SYSTEM_ID = ALL_SYSTEM_ID[6::13]
+SYSTEM_ID = np.array(list(range(80)) + list(range(721, 801)))
+np.random.default_rng(seed=SEED).shuffle(SYSTEM_ID)  # shuffle first?
 SYSTEM = [ALL_SYSTEM[A] for A in SYSTEM_ID]
-
+N_TRAIN = 128
 ALL_SUBSET_ID = [
-    SYSTEM_ID[:8],  # train subsets: [2, 4, 8, 16]
-    SYSTEM_ID[-5:-3],  # 2 x val
-    SYSTEM_ID[-3:],  # 3 x test
+    SYSTEM_ID[:N_TRAIN],  # train subsets
+    SYSTEM_ID[-50:-40],  # 10 x val
+    np.array(list(range(80, 90)) + list(range(801, 811))),  # 20 x test
 ]
 CROSSVAL = None
 
@@ -48,10 +49,6 @@ CROSSVAL = None
 # }
 
 # ===== RELATIVE DIRS =====
-SCF_DIR = lambda A: join(DATA_DIR, f"{A}")
-RI_DIR = lambda A: join(SCF_DIR(A), RI_FIT_ID)
-REBUILD_DIR = lambda A: join(RI_DIR(A), "rebuild")
-PROCESSED_DIR = lambda A: join(RI_DIR(A), "processed")
 CHKPT_DIR = lambda epoch: join(ML_DIR, "checkpoint", f"epoch_{epoch}")
 EVAL_DIR = lambda A, epoch: join(ML_DIR, "evaluation", f"epoch_{epoch}", f"{A}")
 
@@ -68,12 +65,12 @@ if not exists(join(ML_DIR, "evaluation")):
 
 # ===== HPC =====
 SBATCH = {
-    "job-name": "si_mask",
+    "job-name": "ArgAu_ML",
     "nodes": 1,
-    "time": "1:00:00",
-    "mem-per-cpu": 2000,
+    "time": "6:00:00",
+    # "mem-per-cpu": 2000,
     "partition": "standard",
-    "ntasks-per-node": 20,
+    "ntasks-per-node": 72,
 }
 HPC = {
     "load_modules": ["intel", "intel-oneapi-mpi", "intel-oneapi-mkl"],
@@ -97,10 +94,13 @@ TORCH = {
 TARGET_BASIS = unpickle_dict(  # Load the RI basis set definition
     join(PROCESSED_DIR(SYSTEM_ID[0]), "calc_info.pickle")
 )["basis_set"]
+ATOM_TYPES = [1, 6, 7, 8, 79]  # H C N O Au
+for a in ATOM_TYPES:  # need a basis set definition for all global atoms
+    assert convert.NUM_TO_SYM[a] in TARGET_BASIS["lmax"]
 LMAX = max(TARGET_BASIS["lmax"].values())
 DESCRIPTOR_HYPERS = {
     "spherical_expansion_hypers": {
-        "cutoff": 6.0,  # Angstrom
+        "cutoff": 10.0,  # Angstrom
         "max_radial": 10,  # Exclusive
         "max_angular": LMAX,  # Inclusive
         "atomic_gaussian_width": 0.3,
@@ -114,99 +114,130 @@ DESCRIPTOR_HYPERS = {
         "angular_cutoff": None,
         "selected_keys": mts.Labels(
             names=["o3_lambda", "o3_sigma"],
-            values=torch.tensor([[l, 1] for l in np.arange(LMAX + 1)]),
+            values=torch.tensor([[l, 1] for l in torch.arange(LMAX + 1)]),
         ),
         "skip_redundant": True,
     },
-    "atom_types": [1, 14],
-    "mask_descriptor": False,
-    "slab_depth": 4.0,  # Ang
-    "interphase_depth": 1.0,  # Ang
+    "atom_types": ATOM_TYPES,
+    "mask_descriptor": True,
 }
+if MASK is not None and DESCRIPTOR_HYPERS["mask_descriptor"] is True:
+    DESCRIPTOR_HYPERS.update(MASK)  # i.e. `surface_depth` and `buffer_depth`
 
 
 def net(
     in_keys: mts.Labels,
-    invariant_key_idxs: List[int],
     in_properties: mts.Labels,
     out_properties: mts.Labels,
     dtype: torch.dtype,
     device: torch.device,
 ) -> torch.nn.Module:
-    """Builds a NN sequential ModuleMap"""
-    return nn.Sequential(
+    """
+    Builds a NN sequential ModuleMap
+    """
+    all_invariant_key_idxs = [
+        i for i, key in enumerate(in_keys) if key["o3_lambda"] == 0
+    ]
+    # au_invariant_key_idxs = [
+    #     i
+    #     for i, key in enumerate(in_keys)
+    #     if key["o3_lambda"] == 0 and key["center_type"] == 79
+    # ]
+    sequential = nn.Sequential(
         in_keys,
-        nn.InvariantLayerNorm(
+        nn.InvariantLayerNorm(  # for all invariants
             in_keys=in_keys,
-            invariant_key_idxs=invariant_key_idxs,
+            invariant_key_idxs=all_invariant_key_idxs,
             in_features=[
                 len(in_props)
                 for i, in_props in enumerate(in_properties)
-                if i in invariant_key_idxs
+                if i in all_invariant_key_idxs
             ],
             dtype=dtype,
             device=device,
         ),
-        nn.EquivariantLinear(
+        nn.EquivariantLinear(  # Linear layer for all blocks
             in_keys=in_keys,
-            invariant_key_idxs=invariant_key_idxs,
+            invariant_key_idxs=all_invariant_key_idxs,
             in_features=[len(in_props) for in_props in in_properties],
-            out_features=128,
-            bias=True,  # equivariant bias
-            dtype=dtype,
-            device=device,
-        ),
-        nn.InvariantSiLU(in_keys=in_keys, invariant_key_idxs=invariant_key_idxs),
-        nn.EquivariantLinear(
-            in_keys=in_keys,
-            invariant_key_idxs=invariant_key_idxs,
-            in_features=128,
+            # out_features=32,
             out_properties=out_properties,
             bias=True,  # equivariant bias
             dtype=dtype,
             device=device,
         ),
+        # nn.InvariantTanh(  # activation only for Au invariants
+        #     in_keys=in_keys, invariant_key_idxs=au_invariant_key_idxs, out_properties=out_properties,
+        # ),
+        # nn.EquivariantLinear(
+        #     in_keys=in_keys,
+        #     invariant_key_idxs=all_invariant_key_idxs,
+        #     in_features=32,
+        #     out_properties=out_properties,
+        #     bias=True,  # equivariant bias
+        #     dtype=dtype,
+        #     device=device,
+        # ),
     )
+
+    # for key, block_nn in zip(in_keys, sequential.module_map):
+
+        # # Initialize Au weights to zero.
+        # if key["center_type"] == 79:
+        #     for i, param in enumerate(block_nn.parameters()):
+        #         param.data = torch.nn.Parameter(torch.zeros_like(param))
+
+        # # Freeze the other weights
+        # else:
+        #     for param in block_nn.parameters():
+        #         param.requires_grad = False
+
+    return sequential
 
 
 NET = net
+PRETRAINED_MODEL = None
 TRAIN = {
     # Training
-    "batch_size": 1,
-    "n_epochs": 2001,  # number of total epochs to run. End in a 1, i.e. 20001.
+    "batch_size": 4,
+    "n_epochs": 10001,  # number of total epochs to run. End in a 1, i.e. 20001.
     "restart_epoch": None,  # The epoch of the last saved checkpoint, or None for no restart
     "use_overlap": False,  # True/False, or the epoch to start using it at
+    # "overlap_type": "diag",
     # Saving and logging
     "checkpoint_interval": 500,  # save model and optimizer state every x intervals
     "log_interval": 500,  # how often to log the loss
     "log_block_loss": True,  # whether to log the block losses
 }
-OPTIMIZER = partial(torch.optim.Adam, **{"lr": 1e-2})
+OPTIMIZER = partial(torch.optim.Adam, **{"lr": 1e-1})
 # SCHEDULER = None
-SCHEDULER = partial(
-    torch.optim.lr_scheduler.MultiStepLR,
-    **{
-        "milestones": [500, 1000, 1500],
-        "gamma": 0.1,
-    },
-)
-LOSS_FN = L2Loss
+SCHEDULER = partial(torch.optim.lr_scheduler.StepLR, **{"step_size": 1000, "gamma": 0.1})
+TRAIN_LOSS_FN = L2Loss(overlap_type=None)
+VAL_LOSS_FN = L2Loss(overlap_type="on-site")
 EVAL = {  # Evaluation
     "eval_id": ALL_SUBSET_ID[2],
-    "eval_epoch": 2000,
+    "eval_epoch": 10000,
     "target_type": "ri",  # evaluate MAE against QM ("ref") or RI ("ri") scalar field
     "evaluate_on": "surface",  # "surface" or "slab"
-    "stm": {
-        "center_coord": 0,  # Angstrom, offset from max Z coord
-        "thickness": 4.0,  # Angstrom
-    },
 }
-
+DATA_NAMES = {
+    "target": (
+        "ri_coeffs.npz"
+        if DESCRIPTOR_HYPERS["mask_descriptor"] is False
+        else f"ri_coeffs_masked_{MASK['surface_depth']}_{MASK['buffer_depth']}.npz"
+    ),
+    "aux": (
+        "ri_ovlp.npz"
+        if DESCRIPTOR_HYPERS["mask_descriptor"] is False
+        else f"ri_ovlp_masked_{MASK['surface_depth']}_{MASK['buffer_depth']}.npz"
+    ),
+}
+if TRAIN.get("overlap_type") == "diag":
+    DATA_NAMES["aux_diag"] = DATA_NAMES["aux"].replace(".npz", "_diag.npz")
 
 # ===== FINAL DICT =====
 ML_SETTINGS = {
     "SEED": SEED,
-    "TOP_DIR": TOP_DIR,
     "DATA_DIR": DATA_DIR,
     "FIELD_NAME": FIELD_NAME,
     "RI_FIT_ID": RI_FIT_ID,
@@ -225,15 +256,18 @@ ML_SETTINGS = {
     "TARGET_BASIS": TARGET_BASIS,
     "DESCRIPTOR_HYPERS": DESCRIPTOR_HYPERS,
     "NET": NET,
+    "PRETRAINED_MODEL": PRETRAINED_MODEL,
     "TRAIN": TRAIN,
     "OPTIMIZER": OPTIMIZER,
     "SCHEDULER": SCHEDULER,
-    "LOSS_FN": LOSS_FN,
+    "TRAIN_LOSS_FN": TRAIN_LOSS_FN,
+    "VAL_LOSS_FN": VAL_LOSS_FN,
+    "DATA_NAMES": DATA_NAMES,
+    "EVAL": EVAL,
     "SCF_DIR": SCF_DIR,
     "RI_DIR": RI_DIR,
-    "REBUILD_DIR": REBUILD_DIR,
     "PROCESSED_DIR": PROCESSED_DIR,
+    "REBUILD_DIR": REBUILD_DIR,
     "CHKPT_DIR": CHKPT_DIR,
     "EVAL_DIR": EVAL_DIR,
-    "EVAL": EVAL,
 }
